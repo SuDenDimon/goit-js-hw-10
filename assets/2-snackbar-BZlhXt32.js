c7baa7e1476a69c14e4ec7545572893d12effcbb
SuDenDimon <94171603+SuDenDimon@users.noreply.github.com>
2025-03-09 20:18:10 +0000

    Deploying to gh-pages from @ SuDenDimon/goit-js-hw-10@aa1f74c45aaa73ef591dbc4ce18d272591cdbd87 🚀

---
+++ assets/2-snackbar-BZlhXt32.js
@@ -0,0 +1,2 @@
+import{i as s}from"./vendor-BbSUbo7J.js";const l=document.querySelector(".form");l.addEventListener("submit",o=>{o.preventDefault();const e=o.currentTarget.elements.delay.value,t=o.currentTarget.elements.state.value;new Promise((r,i)=>{setTimeout(()=>{console.log(e,t),t==="fulfilled"?r(e):i(e)},e)}).then(r=>{s.success({color:"green",position:"topRight",message:`✅ Fulfilled promise in ${e}ms`})}).catch(r=>{s.error({color:"red",position:"topRight",message:`❌ Rejected promise in ${e}ms`})})});
+//# sourceMappingURL=2-snackbar-BZlhXt32.js.map
